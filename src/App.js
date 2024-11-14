db54d5cab2f9c4fb4d1865432a6dfc3482e895a7
last-ent <last_ent@outlook.com>
2024-11-14 11:05:38 +0100

    simple poc

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { StatsigClient} from '@statsig/js-client';
+
+
 function App() {
+
+  const YOUR_SDK_KEY = "";
+
+  const user = { userID: 'some_user_id-333' };
+  const options = {
+    environment: { tier: "development" }
+  }
+  
+  const myStatsigClient = new StatsigClient(YOUR_SDK_KEY, user, options);
+  myStatsigClient.initializeSync();
+  
+  const homepageStore = myStatsigClient.getParameterStore("nik_test_1");
+  console.log(homepageStore)
+
+  // console.log(homepageStore.get("button1"))
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {homepageStore.get("tag1", "default")}
         </p>
         <a
           className="App-link"
